ab7abadc6534518acb4f3c2a8bdd45a89a4246ce
SERAP KANTAROĞLU <serapkantaroglu.59@hotmail.com>
2022-05-20 22:26:13 +0300

    HomeComponent

--- Angular-Service/src/app/app-routing.module.ts
+++ Angular-Service/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {
+    path:"home",component:HomeComponent
+
+  },
   {
     path:"map",
     loadChildren:()=>
     import("./map/map.module").then(m=>m.MapModule)
   }
+
 ];
 
 @NgModule({
